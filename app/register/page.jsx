33f46cc8d40dc2d80a6f2494cbb0fcca020d86ed
Alishah Safdari <alishahsafdari905@gmail.com>
2024-08-05 14:56:59 +0430

    Fix the bug

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,12 +1,13 @@
 "use client"
 
 import RegisterForm from '@/components/RegisterForm'
-import React from 'react'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '../api/auth/[...nextauth]/route'
 
-export default function Register() {
-  return (
-    <div>
-      <RegisterForm/>
-    </div>
-  )
+export default async function Register() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/dashboard");
+
+  return (<RegisterForm />)
 }
